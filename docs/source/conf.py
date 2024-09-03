46c12a927b1f03b1e2de905e0b97d3fd973716c8
Damien Cauquil <virtualabs@gmail.com>
2024-09-03 14:33:14 +0200

    Add WHAD logo to documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,8 +19,8 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Wireless HAcking Devices Protocol'
-copyright = '2022, Damien Cauquil, Romain Cayre'
+project = 'WHAD Protocol'
+copyright = '2024, Damien Cauquil, Romain Cayre'
 author = 'Damien Cauquil, Romain Cayre'
 
 # The short X.Y version
@@ -42,7 +42,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
     'sphinx_rtd_theme',
-    'sphinxcontrib.mermaid'
+    'sphinxmermaid'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -84,7 +84,10 @@ html_theme = 'sphinx_rtd_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_logo = "images/logo.png"
+html_theme_options = {
+    "logo_only": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
